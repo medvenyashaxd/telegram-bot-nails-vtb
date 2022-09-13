fbb55d8e189469450d2ab3f7fcd6b3213767721b
medvenyashaXD <xmedvenyashax@gmail.com>
2022-09-13 18:14:34 +0300

    added handler for admin

--- data_for_the_bot.py
+++ data_for_the_bot.py
@@ -2,9 +2,10 @@ import os
 
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot_aiogram = Bot(token=os.getenv('TOKEN'))
 
+memory_storage = MemoryStorage()
 
-dp = Dispatcher(bot_aiogram)
+dp = Dispatcher(bot_aiogram, storage=memory_storage)
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,13 @@
 from aiogram.utils import executor
 from data_for_the_bot import dp
-from handlers import for_client
+from handlers import for_client, for_admin
 
 
 async def show_launch_notification(_):
     print('Bot started!')
 
 for_client.register_handler_client(dp)
+for_admin.register_handlers_admin(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=show_launch_notification)
 
